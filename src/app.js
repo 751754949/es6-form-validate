08a992d6d857086546c919117a8551290cf0f459
Yonghang Song <songyh5@asiainfo.com>
2017-08-02 16:59:42 +0800

    init

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 
 //错误信息提示重置
-form.reset('warn', $.toptip)
+// form.reset('warn', $.toptip)
 
 //柯里化
 var toast = function(type) {
@@ -11,7 +11,7 @@ var toast = function(type) {
         $.toast(msg, type)
     }
 }
-// form.reset('warn', toast('forbidden'))
+form.reset('warn', toast('forbidden'))
 
 //把默认的'.has-error'重置成'.error'
 
@@ -21,12 +21,17 @@ form.register({
     REG_NUMBER: /^\d+\.\d{2}$/
 })
 
-// form.reset('errorHandler', ($input)=>{
-//     $input.closest('.form-group').addClass('has-error')
-// }) 
+form.reset('errorHandler', ($input)=>{
+    $input.closest('.weui-cell').addClass('weui-cell_warn').removeClass('weui-cell_success')
+}) 
+
+form.reset('successHandler', ($input)=>{
+    $input.closest('.weui-cell').addClass('weui-cell_success').removeClass('weui-cell_warn')
+}) 
+
 var page = {
     validate() {
-        form.validate('.valid');
+        form.validate('body');
     }
 }
 export {
